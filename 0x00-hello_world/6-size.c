ad7f4a4b516a5b03197e0efb47792a83d97bfa5b
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-06-30 14:57:10 -0700

    6-size.c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
+#include <stdio.h>
 /**
  * main - this fuction prints the size of various types
- * Return: it returns 0 after running successfully
+ * Return: 0 if properly exited, non-zero otherwise
  */
-#include <stdio.h>
 int main(void)
 {
-	prinf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	pritf("size of a long int: %zu byte(s)\n", sizeof(long));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	prinf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	pritf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
