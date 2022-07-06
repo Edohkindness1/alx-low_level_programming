0e42f4c0e6334d4bb9409f7ecb5e249cb5af9ce3
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-07-06 05:06:04 -0700

    largets_number task

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - prints the largest of 3 numbers
+ *  @x: first integer
+ *  @y: second integer
+ *  @z: third integer
+ *  Return: largest integer
+ */
+
+int largest_number(int 1, int 2, int 3)
+{
+	int largets;
+
+	if (x > y && x > z)
+	{
+		largest = x;
+	}
+	else if (y > x && y < z)
+	{
+		largest = y;
+	}
+	else if (z > x && z > y)
+	{
+		largest = z;
+	}
+	return (largest);
+}
