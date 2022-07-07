ed4b1886642195b5d106a6d824d3d3afa90e3b0e
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-07-07 05:13:04 -0700

    numbers speak for themselves task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - print from 0 to 9
- *
- * Return: void
- */
+*print_numbers - print from 0 to 9
+*
+*Return: void
+*/
 
 void print_numbers(void)
 {
@@ -12,9 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }
