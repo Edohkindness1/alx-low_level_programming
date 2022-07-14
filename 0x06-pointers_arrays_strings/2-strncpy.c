01af3256bf6044115b1e03c98d137725ac8c95b7
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-07-14 03:52:48 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,15 @@
  *
  * Return: poiter to the resulting string dest
  */
-char *strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && *(src + i); i++)
+	for (i = 0; i < n && *(src + i); i+=)
 	{
 		*(dest + i) = *(src + i);
 	}
-	for  (; i < n; i++)
+	for (; i < n; i++)
 	{
 		*(dest + i) = '\0';
 	}
