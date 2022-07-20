b67d0522c33f96caae245322543047ca8a236476
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-07-20 02:05:31 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,11 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
   * _puts_recursion - prints a string, followed by a new line
   * @s: pointer to string
   * Return: nothing
-  **/
+  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,5 +14,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif
+#endif/*MAIN_H*/
