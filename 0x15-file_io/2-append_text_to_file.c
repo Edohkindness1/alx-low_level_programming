06c1c730d0264747f8445a09aa2d908c39054c6c
Edohkindness1 <ibrahimedoh8@gmail.com>
2022-08-22 06:01:31 -0700

    a function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,3 +24,18 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	return (1);
 }
+
+/**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
